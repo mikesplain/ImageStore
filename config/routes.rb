c04404b5fe318ab7b70ae24ff3b978ec49cb8cdd
Mike Splain <mike.splain@gmail.com>
2012-11-06 17:56:51 -0500

    Add Image scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Imagestore::Application.routes.draw do
+  resources :images
+
   resources :folders
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106034615) do
+ActiveRecord::Schema.define(:version => 20121106225502) do
 
   create_table "folders", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(:version => 20121106034615) do
     t.string   "url"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "images", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
